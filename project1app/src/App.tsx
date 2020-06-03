9307489d225b92ad8ac8435a8bc443b6fe224737
kruti4 <krutifaldu9@gmail.com>
2020-06-03 10:44:12 -0700

    update app.txt

--- project1app/src/App.tsx
+++ project1app/src/App.tsx
@@ -1,61 +1,60 @@
-import React from 'react';
-import { NavbarComponent } from './components/Navigation';
-import { LoginComponent } from './components/Login';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { User } from './models/Users';
+import React from "react";
+import { NavbarComponent } from "./components/Navigation";
+import { LoginComponent } from "./components/Login";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { User } from "./models/Users";
+import { AllUsers } from "./components/allUsers";
+import { MultiRoute } from "./components/multiRoutReimbursement";
 
 interface IAppState {
   loggedInUser: User | null;
 }
 
 export class App extends React.Component<any, IAppState> {
-
   constructor(props: any) {
     super(props);
     this.state = {
       loggedInUser: null,
-    }
+    };
   }
 
   updateUser = (user: User) => {
     console.log(user);
     this.setState({
       loggedInUser: user,
-    })
-  }
+    });
+  };
 
   render() {
     return (
       <>
         <div>
           <h3>hello</h3>
-          <h1>Hello {this.state.loggedInUser ? this.state.loggedInUser.username : 'guest'}</h1>
+          <h1>
+            Hello{" "}
+            {this.state.loggedInUser
+              ? this.state.loggedInUser.username
+              : "guest"}
+          </h1>
         </div>
         <Router>
-
           <NavbarComponent />
           <Switch>
             {/* <Route path='/home' >
-
             </Route> */}
-
-            <Route path='/users'>
-
-            </Route>
-            <Route path="/reimbursement">
-
-            </Route>
-            <Route path='/login' >
+            <Route path="/login">
               <LoginComponent updateUser={this.updateUser} />
             </Route>
+            <Route
+              path="/users"
+              component={AllUsers}
+              users={this.updateUser}
+            ></Route>
+            <Route path="/reimbursement" component={MultiRoute}></Route>
 
-            <Route path='/logout' >
-
-            </Route>
-
+            <Route path="/logout"></Route>
           </Switch>
         </Router>
-
       </>
     );
   }
